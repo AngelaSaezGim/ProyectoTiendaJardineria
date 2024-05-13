a32ea5bc4bb96db754fe9493689782df65997b53
AngelaSaezGim <angelasaezgim12@gmail.com>
2024-05-13 10:42:58 +0200

    Commit condicionales hechos

--- src/tiendaDataAccess/DataAccessManager.java
+++ src/tiendaDataAccess/DataAccessManager.java
@@ -3,11 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tiendaDataAccess;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static tiendaDataAccessConstants.Constants.*;
 
 /**
  *
  * @author angsaegim
  */
 public class DataAccessManager {
+    
+     /************************** PARTE ESTÁTICA *****************************/
+    private static String dataBaseUser = DEFAULT_DATA_BASE__USER;
+    private static String dataBasePwd = DEFAULT_DATA_BASE__PWD;
+    private static String dataBaseURL = DEFAULT_DATA_BASE__URL;
+    
+    private static DataAccessManager singleton;
+    
+     // Instanciamos un único objeto DataAccessManager - SINGLETON
+    
+    private DataAccessManager(){
+        
+    }
     
 }
---
+++ src/tiendaDataAccessConstants/Constants.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tiendaDataAccessConstants;
+
+import java.io.File;
+
+/**
+ *
+ * @author angsaegim
+ */
+public class Constants {
+    
+    //path relativo del fichero de configuración que incluye las credenciales y URL configurables
+    public final static String DB_CONFIG__FILE_NAME = "resources" + File.separator + "db.properties";
+    
+    public final static String MYSQL_DB_DRIVER__CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    
+    //propiedades configurables en el fichero de configuración
+    public final static String DB_CONFIG__USER_PROPERTY = "user";
+    public final static String DB_CONFIG__PWD_PROPERTY = "pass";
+    public final static String DB_CONFIG__URL_PROPERTY = "url";
+    
+    //Valores por defecto de credenciales y URL de acceso a datos 
+    //(utilizadas en caso de error en la carga del fichero de configuración)
+    public final static String DEFAULT_DATA_BASE__USER = "tester";
+    public final static String DEFAULT_DATA_BASE__PWD = "tester";
+    public final static String DEFAULT_DATA_BASE__URL = "jdbc:mysql://localhost:3306/jardineriaJava";
+}
