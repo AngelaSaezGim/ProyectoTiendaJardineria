92b8225b1e6a5da89fd291c0a48ab1fb7599914d
AngelaSaezGim <angelasaezgim12@gmail.com>
2024-05-13 10:35:24 +0200

    Commit condicionales hechos

---
+++ src/tiendaApp/TiendaApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tiendaApp;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author angsaegim
+ */
+public class TiendaApp {
+    
+    private static Scanner tcl = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
